89a7aee739bf5c1ef4d8b3a8bcb95a0f52380016
ngjupeng <jupeng2015@gmail.com>
2025-09-18 15:13:14 +0700

    docs production url

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/icon-starknet.svg",
 
   // Set the production url of your site here
-  url: "https://qash.finance/",
+  url: "https://app.qash.finance/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/docs/",
