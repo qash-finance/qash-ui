f97ce77f0c38554d98103ce3f617fc6abb9ee366
ngjupeng <jupeng2015@gmail.com>
2026-01-13 11:54:48 +0000

    feat: full screen loading

--- packages/nextjs/components/Loading/FullScreenLoading.tsx
+++ packages/nextjs/components/Loading/FullScreenLoading.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import Image from "next/image";
 
 export default function FullScreenLoading() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-background">
-      <div className="w-12 h-12 border-4 border-primary-blue border-t-transparent rounded-full animate-spin" />
+    <div className="fixed inset-0 z-50 bg-white">
+      <div className="absolute left-0 top-0 h-4 animate-[progress_2s_ease-in-out_infinite] bg-[#066EFF]" />
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Image
+          src="/logo/loading.gif"
+          alt="Loading"
+          width={450}
+          height={450}
+          priority
+          unoptimized
+        />
+      </div>
+      <style jsx>{`
+        @keyframes progress {
+          0% {
+            width: 0;
+          }
+          100% {
+            width: 100%;
+          }
+        }
+      `}</style>
     </div>
   );
 }
